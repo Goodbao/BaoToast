b35e2854efde030ebdcba851516beb00dbabe453
Android_wyh <bao872953259@miriding.com>
2018-12-21 18:15:26 +0800

    GIF

--- app/src/main/java/com/bao/baotoast/MainActivity.kt
+++ app/src/main/java/com/bao/baotoast/MainActivity.kt
@@ -11,26 +11,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btnShortToast).setOnClickListener {
-            ToastUtils.shortToast(R.string.normal_toast)
-        }
+findViewById<Button>(R.id.btnShortToast).setOnClickListener {
+    ToastUtils.shortToast(R.string.normal_toast)
+}
 
-        findViewById<Button>(R.id.btnLongToast).setOnClickListener {
-            ToastUtils.longToast(getString(R.string.long_toast))
-        }
+findViewById<Button>(R.id.btnLongToast).setOnClickListener {
+    ToastUtils.longToast(getString(R.string.long_toast))
+}
 
-        findViewById<Button>(R.id.btnCenterToast).setOnClickListener {
-            ToastUtils.setToastGravity(Gravity.CENTER, 0, 0)
-            ToastUtils.shortToast(R.string.center_toast)
-        }
+findViewById<Button>(R.id.btnCenterToast).setOnClickListener {
+    ToastUtils.setToastGravity(Gravity.CENTER, 0, 0)
+    ToastUtils.shortToast(R.string.center_toast)
+}
 
-        findViewById<Button>(R.id.btnBottomToast).setOnClickListener {
-            ToastUtils.setToastGravity(Gravity.BOTTOM, 0, 100)
-            ToastUtils.shortToast(R.string.bottom_toast)
-        }
+findViewById<Button>(R.id.btnBottomToast).setOnClickListener {
+    ToastUtils.setToastGravity(Gravity.BOTTOM, 0, 100)
+    ToastUtils.shortToast(R.string.bottom_toast)
+}
 
-        findViewById<Button>(R.id.btnImageToast).setOnClickListener {
-            ToastUtils.shortImageToast(R.string.image_toast)
-        }
+findViewById<Button>(R.id.btnImageToast).setOnClickListener {
+    ToastUtils.shortImageToast(R.string.image_toast)
+}
     }
 }
